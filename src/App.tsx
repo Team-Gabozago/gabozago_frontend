97d7fed2780a90ed0b395a34d9f7be93571d8cce
Muffin <jinlog9@gmail.com>
2022-10-29 09:26:51 +0900

    🛠 Settings 구축 진행 .
    1. yarn berry setting
    2. router setting
    3. webpack setting

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { lazy, Suspense } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+const HomePage = lazy(() => import('@/pages/Home'));
+
+export default function App() {
+    return (
+        <BrowserRouter>
+            <Suspense fallback={null}>
+                <Routes>
+                    <Route path="/" element={<HomePage />} />
+                </Routes>
+            </Suspense>
+        </BrowserRouter>
+    );
+}
---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,5 @@
+import * as S from './Home.style';
+
+export default function HomePage() {
+    return <S.HomeContainer>Home Page</S.HomeContainer>
+}
