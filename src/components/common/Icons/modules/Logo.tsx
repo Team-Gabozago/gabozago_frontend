e722392c7874ff6dba39fc1ab85abcc1313d34bb
Muffin <jinlog9@gmail.com>
2022-11-07 11:05:58 +0900

    🚧 Icon 컴포넌트화 -> 로고만 따로 테스트 해봄 http://localhost:8080/icons 페이지에서 모든 아이콘들 확인가능하도록 관리자용 페이지를 하나 만들었습니다.

---
+++ src/components/common/Icons/modules/Logo.tsx
@@ -0,0 +1,10 @@
+import * as S from './style';
+
+const Small = () => <S.Logo className="ic-logo" fontSize={2.5} />
+
+const Big = () => <S.Logo className="ic-logo" fontSize={4.5} />
+
+export default {
+    Small,
+    Big,
+};
