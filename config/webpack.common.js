6d7489d1db6912122a5a0d0b758b341701e7ca8b
Muffin <jinlog9@gmail.com>
2022-11-10 01:19:11 +0900

    🛠 emotion 관련 셋팅 수정, 컴포넌트에 인라인으로 css 적용하기 및 styled 안에서 외부 컴포넌트 접근 가능하여 스타일 지정하기

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -10,46 +10,55 @@ const isDevelopment = process.env.NODE_ENV === 'development';
 dotenv.config({ path: path.join(__dirname, '../env', '.env') });
 
 module.exports = {
-  context: __dirname,
-  entry: '../src/index.tsx',
-  output: {
-    filename: '[name].js',
-    path: path.resolve(__dirname, '../build'),
-    clean: true,
-    assetModuleFilename: 'assets/[hash][ext][query]',
-    publicPath: '/',
-  },
-  resolve: {
-    extensions: ['.tsx', '.ts', '.jsx', '.js'],
-    plugins: [new TsconfigPathsPlugin()],
-  },
-  module: {
-    rules: [
-      {
-        test: /\.[jt]sx?$/,
-        exclude: /(node_modules)/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            plugins: [
-              isDevelopment && require.resolve('react-refresh/babel'),
-            ].filter(Boolean),
-          },
-        },
-      },
-      {
-        test: /\.(png|svg|jpg|jpeg|gif|ico|woff|woff2|eot|ttf|otf)$/i,
-        type: 'asset/resource',
-      },
-    ],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: '../public/index.html',
-    }),
-    new webpack.DefinePlugin({
-      'process.env': JSON.stringify(process.env),
-    }),
-    isDevelopment && new ReactRefreshWebpackPlugin(),
-  ].filter(Boolean),
+    context: __dirname,
+    entry: '../src/index.tsx',
+    output: {
+        filename: '[name].js',
+        path: path.resolve(__dirname, '../build'),
+        clean: true,
+        assetModuleFilename: 'assets/[hash][ext][query]',
+        publicPath: '/',
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.jsx', '.js'],
+        plugins: [new TsconfigPathsPlugin()],
+    },
+    module: {
+        rules: [
+            {
+                test: /\.[jt]sx?$/,
+                exclude: /(node_modules)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        plugins: [
+                            [
+                                '@emotion/babel-plugin',
+                                {
+                                    autoLabel: 'dev-only',
+                                    labelFormat:
+                                        'DEV_[dirname]_[filename]_[local]',
+                                },
+                            ],
+                            isDevelopment &&
+                                require.resolve('react-refresh/babel'),
+                        ].filter(Boolean),
+                    },
+                },
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif|ico|woff|woff2|eot|ttf|otf)$/i,
+                type: 'asset/resource',
+            },
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: '../public/index.html',
+        }),
+        new webpack.DefinePlugin({
+            'process.env': JSON.stringify(process.env),
+        }),
+        isDevelopment && new ReactRefreshWebpackPlugin(),
+    ].filter(Boolean),
 };
--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,13 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 const HomePage = lazy(() => import('@/pages/Home'));
 const IconPage = lazy(() => import('@/pages/Icons'));
-
+const SignupPage = lazy(() => import('@/pages/Signup'));
 export default function App() {
     return (
         <BrowserRouter>
             <Suspense fallback={null}>
                 <Routes>
+                    <Route path="/signup" element={<SignupPage />} />
                     <Route path="/icons" element={<IconPage />} />
                     <Route path="/" element={<HomePage />} />
                 </Routes>
