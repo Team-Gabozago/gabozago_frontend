8adc78f48434b7d4d64bdf645c570b9723a64ace
Muffin <jinlog9@gmail.com>
2022-11-10 11:52:51 +0900

    Fe feature/#17 signup form
    * 🚧 FE: Signup Form 기능 구현중 - + Form Check
    
    * 🚧 FE: Signup Page Form 값 체킹 코드 모두 적용, title 동적으로 수정 완료
    
    => 다음 버튼을 누르고 이전에 작성했던 내용을 지운 상태에서도 다음 버튼이 활성화 되는 문제 해결 필요.
    
    * 🚧 FE: Singup Form Error Text 추가, 인풋에서 EnterKey 새로고침 방지

---
+++ src/pages/Signup/Signup.stlye.ts
@@ -0,0 +1,33 @@
+import styled from '@emotion/styled';
+
+import { flexbox } from '@/styles/mixin';
+import theme from '@/styles/theme';
+
+export const Title = styled.h1`
+    width: 18.5rem;
+    font-size: ${theme.fontSize.title};
+    font-weight: bold;
+`;
+
+export const DirectiveMsg = styled.span<{ active: boolean }>`
+    width: 100%;
+    margin-top: 1.6rem;
+    margin-left: 1.7rem;
+    font-size: ${theme.fontSize.xs};
+    color: ${props =>
+        props.active ? theme.color.gray : theme.color.errorText};
+    /* transform: translate3d(0.9rem, 0, 0); */
+`;
+
+export const ButtonWrapper = styled.div`
+    margin-top: 2rem;
+`;
+
+export const SignupForm = styled.form`
+    ${flexbox({ dir: 'column', ai: 'center' })}
+    gap: 1rem;
+`;
+
+export const SignupWrapper = styled.section`
+    padding: 2rem;
+`;
