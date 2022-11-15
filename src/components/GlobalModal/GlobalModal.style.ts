5f0eca5325fb08ca00642ea99a006793f3d8dd9e
Muffin <jinlog9@gmail.com>
2022-11-15 12:46:22 +0900

    🚧 FE: 이메일 중복확인 기능 구현

---
+++ src/components/GlobalModal/GlobalModal.style.ts
@@ -0,0 +1,50 @@
+import { css } from '@emotion/react';
+import styled from '@emotion/styled';
+
+import { flexbox } from '@/styles/mixin';
+import theme from '@/styles/theme';
+
+export const Overlay = styled.div`
+    position: fixed;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+    z-index: 999;
+    background-color: #000;
+    opacity: 0.6;
+`;
+
+const selectSize = (size: string) => {
+    switch (size) {
+        case 'small':
+            return css`
+                width: 20rem;
+                height: 12rem;
+            `;
+        case 'medium':
+            return css`
+                width: 25rem;
+                height: 32rem;
+            `;
+        case 'large':
+            return css`
+                width: 32rem;
+                height: 40rem;
+            `;
+        default:
+            throw new Error(`${size} type is not found`);
+    }
+};
+
+export const GlobalModal = styled.section<{ size: string }>`
+    ${({ size }) => selectSize(size)}
+    ${flexbox({ dir: 'column', ai: 'center' })}
+  position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    background-color: ${theme.color.white};
+    z-index: 999;
+    border-radius: 2rem;
+`;
