03af8a0511e6b44571e69c7000c4cb054a6e1b39
Muffin <jinlog9@gmail.com>
2022-11-29 16:03:30 +0900

    🚧 FE: 마이페이지 - 패스워드 변경 기능 구현 진행 완료, 응답데이터 처리만 남음

--- src/apis/mypage.ts
+++ src/apis/mypage.ts
@@ -22,26 +22,55 @@ export const getMyPage = async () => {
     }
 };
 
-export const getMyPassword = async () => {
+export const checkMyPassword = async (currentPassword: string) => {
     const accessToken = localStorage.getItem('accessToken');
     if (!accessToken) throw new Error('accessToken is undefined');
 
     const response = await fetch(
         `${process.env.GABOZAGO_URL}/auth/check-password`,
         {
-            method: 'GET',
+            method: 'POST',
             headers: {
                 Authorization: accessToken,
                 'Content-Type': 'application/json',
             },
+            body: JSON.stringify({ password: currentPassword }),
         }
     );
 
     const myPassword = await response.json();
 
     try {
-        return myPassword;
+        if (myPassword) {
+            return myPassword.correct;
+        }
     } catch (err) {
-        throw new Error(`getMypage get api fail err: ${err}`);
+        throw new Error(`getMyPassword get api fail err: ${err}`);
+    }
+};
+
+export const patchMyPassword = async (
+    currentPassword: string,
+    newPassword: string
+) => {
+    const accessToken = localStorage.getItem('accessToken');
+    if (!accessToken) throw new Error('accessToken is undefined');
+
+    const response = await fetch(`${process.env.GABOZAGO_URL}/auth/password`, {
+        method: 'PATCH',
+        credentials: 'include',
+        headers: {
+            Authorization: accessToken,
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ password: currentPassword, newPassword }),
+    });
+
+    const data = await response.json();
+
+    try {
+        return data;
+    } catch (err) {
+        throw new Error(`patchMyPassword patch api fail err: ${err}`);
     }
 };
