b902409b73260d5f28b120fc486e0e672baa5a7a
muffin9 <jinlog9@gmail.com>
2022-12-01 11:32:32 +0900

    ♻️ FE: 모바일환경에서 깨지는 UI 수정

--- src/styles/GlobalStyle.tsx
+++ src/styles/GlobalStyle.tsx
@@ -1,47 +1,47 @@
 import { Global, css } from '@emotion/react';
 
-import { flexbox } from './mixin';
+import { flexbox } from '@/styles/mixin';
 
 import './reset.css';
 import './font.css';
 
 const style = css`
-  * {
-    box-sizing: border-box;
-  }
-  html {
-    font-size: 1rem;
-  }
-  body {
-    ${flexbox({ jc: 'center' })};
-    box-sizing: border-box;
-    background-color: #f7f7fc;
-    overflow-x: hidden;
-    font-family: 'Pretendard-Regular';
-  }
-  a {
-    color: inherit;
-    text-decoration: none;
-  }
-  select,
-  input,
-  button,
-  textarea {
-    border: 0;
-    outline: 0 !important;
-    padding: 0;
-  }
-  button {
-    cursor: pointer;
-    background-color: transparent;
-  }
-  input[type='number']::-webkit-outer-spin-button,
-  input[type='number']::-webkit-inner-spin-button {
-    -webkit-appearance: none;
-    margin: 0;
-  }
+    * {
+        box-sizing: border-box;
+    }
+    html {
+        font-size: 1rem;
+    }
+    body {
+        ${flexbox({ jc: 'center' })};
+        box-sizing: border-box;
+        background-color: #fff;
+        overflow-x: hidden;
+        font-family: 'Pretendard-Regular';
+    }
+    a {
+        color: inherit;
+        text-decoration: none;
+    }
+    select,
+    input,
+    button,
+    textarea {
+        border: 0;
+        outline: 0 !important;
+        padding: 0;
+    }
+    button {
+        cursor: pointer;
+        background-color: transparent;
+    }
+    input[type='number']::-webkit-outer-spin-button,
+    input[type='number']::-webkit-inner-spin-button {
+        -webkit-appearance: none;
+        margin: 0;
+    }
 `;
 
 export default function GlobalStyle() {
-  return <Global styles={style} />;
+    return <Global styles={style} />;
 }
