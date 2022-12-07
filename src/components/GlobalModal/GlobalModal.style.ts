608056c3f04cd131a77510e03a60da3bb3b9d446
Muffin <jinlog9@gmail.com>
2022-12-07 19:40:32 +0900

    🚧 FE : 피드 삭제 기능 구현 완료, Overlayout 컴포넌트 분리

--- src/components/GlobalModal/GlobalModal.style.ts
+++ src/components/GlobalModal/GlobalModal.style.ts
@@ -4,17 +4,6 @@ import styled from '@emotion/styled';
 import { flexbox } from '@/styles/mixin';
 import theme from '@/styles/theme';
 
-export const Overlay = styled.div`
-    position: fixed;
-    top: 0;
-    right: 0;
-    bottom: 0;
-    left: 0;
-    z-index: 999;
-    background-color: #000;
-    opacity: 0.6;
-`;
-
 const selectSize = (size: string) => {
     switch (size) {
         case 'small':
--- src/pages/Feed/index.tsx
+++ src/pages/Feed/index.tsx
@@ -1,14 +1,18 @@
 import { useQuery, useMutation } from '@tanstack/react-query';
-import { useParams } from 'react-router-dom';
+import { useState } from 'react';
+import { Link, useNavigate, useParams } from 'react-router-dom';
 
 import * as S from './Feed.style';
 
-import { getFeed, unLikeFeed, likeFeed } from '@/apis/feeds';
+import { getFeed, unLikeFeed, likeFeed, deleteFeed } from '@/apis/feeds';
 import I from '@/components/common/Icons';
 import CreateComment from '@/components/Feed/Comment';
 import CommentList from '@/components/Feed/Comment/CommentList';
 import FeedProfile from '@/components/Feed/Profile';
+import GlobalModal from '@/components/GlobalModal';
+import ModalContent from '@/components/ModalContent';
 import Header from '@/components/MyPage/Header';
+import Overlayout from '@/components/OverLayout';
 import theme from '@/styles/theme';
 
 const dummyFeedImages = [
@@ -32,6 +36,9 @@ const dummyFeedImages = [
 
 const FeedPage = () => {
     const { id } = useParams();
+    const navigate = useNavigate();
+    const [isToggleModal, setIsToggleModal] = useState(false);
+    const [isDeleteModal, setIsDeleteModal] = useState(false);
 
     // useQuery.
     const { data: feed, refetch: refetchFeed } = useQuery(['feed'], () => {
@@ -59,6 +66,15 @@ const FeedPage = () => {
         },
     });
 
+    const fetchDeleteFeed = useMutation(deleteFeed, {
+        onSuccess: async () => {
+            navigate('/home');
+        },
+        onError: (error: unknown) => {
+            throw new Error(`error is ${error}`);
+        },
+    })
+
 
     const handleLike = (liked: boolean) => {
         if (liked) {
@@ -68,13 +84,45 @@ const FeedPage = () => {
         }
     };
 
+    const handleToggle = () => {
+        setIsToggleModal(!isToggleModal);
+    }
+
+    const handleDeleteFeed = () => {
+        // delete mutation
+        fetchDeleteFeed.mutate(id);
+    }
+
     return (
         feed && (
             <>
                 <Header title={feed.title} />
                 <S.FeedHeader>
                     <FeedProfile author={feed.author} updatedAt={feed.updatedAt} />
-                    <I.Toggle fontSize={1.5} color={theme.color.gray} />
+                    <S.ToggleButton onClick={handleToggle}>
+                        <I.Toggle fontSize={1.5} color={theme.color.gray} />
+                    </S.ToggleButton>
+                    {isToggleModal &&
+                        <>
+                            <Overlayout handleCancelClick={() => setIsToggleModal(false)} />
+                            <S.ToggleModal>
+                                <Link to="/feed/put">
+                                    <S.ToggleModalBox><I.Edit /> 수정하기</S.ToggleModalBox>
+                                </Link>
+                                <S.ToggleModalBox onClick={() => setIsDeleteModal(true)}><I.Edit /> 삭제하기</S.ToggleModalBox>
+                            </S.ToggleModal>
+                        </>
+                    }
+                    {isDeleteModal &&
+                        <GlobalModal size="small" handleCancelClick={() => setIsDeleteModal(false)}>
+                            <ModalContent
+                                title="해당 피드를 삭제하시겠습니까?"
+                                description="삭제된 피드는 복구 불가능합니다."
+                                buttonText="삭제"
+                                handleButtonClick={handleDeleteFeed}
+                            />
+                        </GlobalModal>
+                    }
                 </S.FeedHeader>
                 <S.FeedContainer>
                     <S.FeedAddress>{feed.location.place} {feed.location.placeDetail}</S.FeedAddress>
