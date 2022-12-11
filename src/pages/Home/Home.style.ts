48dc43b59a4f93a2a4ea8ca630eca0706be87969
Muffin <jinlog9@gmail.com>
2022-12-11 15:35:18 +0900

    FE: 🚧 Home 페이지에서 유저 지역정보 기능 구현하기

--- src/pages/Home/Home.style.ts
+++ src/pages/Home/Home.style.ts
@@ -1,5 +1,6 @@
 import styled from '@emotion/styled';
 
+import { flexbox } from '@/styles/mixin';
 import theme from '@/styles/theme';
 
 export const Title = styled.h1`
@@ -9,6 +10,14 @@ export const Title = styled.h1`
     font-family: 'GangwonEduPower';
     font-weight: bold;
     line-height: 2.2rem;
+`;
+
+export const NotFoundArea = styled.div`
+    ${flexbox({ jc: 'center', ai: 'center' })};
+    height: 100%;
+    line-height: 1.5rem;
+    color: ${theme.color.silver};
+    font-size: ${theme.fontSize.xs};
 `;
 
 export const HomePage = styled.section``;
